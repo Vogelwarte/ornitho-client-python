74749a31acabc31717c6aa89dc8ff84c23e3b107
guidohaefliger <guido.haefliger@vogelwarte.ch>
2026-08-28 15:22:09 +0200

    api_requester: add PUT to method_whitelist

--- ornitho/api_requester.py
+++ ornitho/api_requester.py
@@ -44,7 +44,7 @@ def requests_retry_session(retries=MAX_RETRY_FOR_SESSION,
        retry = Retry(total=retries, read=retries, connect=retries,
                      backoff_factor=back_off_factor,
                      status_forcelist=status_force_list,
-                     method_whitelist=frozenset(['GET', 'POST']))
+                     method_whitelist=frozenset(['GET', 'POST', 'PUT']))
                      # urllib3-version >= 1.26
                      # allowed_methods=frozenset(['GET', 'POST']))
        adapter = HTTPAdapter(max_retries=retry)
